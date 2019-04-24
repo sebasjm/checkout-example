94e082410173c9b246ca0b8ec58847764da398da
Sebastian <sebasjm@gmail.com>
2019-04-24 12:01:39 -0300

    checkout implemented

--- front/pages/index.js
+++ front/pages/index.js
@@ -11,11 +11,12 @@ import DialogActions from '@material-ui/core/DialogActions';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import Link from 'next/link';
+import Checkout from '../src/checkout/Checkout';
 
 const styles = theme => ({
   root: {
     textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
+    // paddingTop: theme.spacing.unit * 20,
   },
 });
 
@@ -42,6 +43,7 @@ class Index extends React.Component {
 
     return (
       <div className={classes.root}>
+        <Checkout />
         <Dialog open={open} onClose={this.handleClose}>
           <DialogTitle>Super Secret Password</DialogTitle>
           <DialogContent>
