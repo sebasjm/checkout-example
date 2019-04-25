f3479e25bf890b4b2da7f2dc18afbe8a02611150
Sebastian <sebasjm@gmail.com>
2019-04-25 00:18:15 -0300

    adding cors

--- back/index.js
+++ back/index.js
@@ -2,8 +2,10 @@ const { prisma } = require('./generated/prisma-client')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 app.use(bodyParser.json())
+app.use(cors())
 
 app.post('/user', async (req, res) => {
   const newUser = await prisma.createUser(req.body)
